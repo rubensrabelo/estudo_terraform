cc5598ff2ef1ad5cf8248588011935a3418a8600
Rubens Rabelo <rubensrabelo123@gmail.com>
2023-11-02 16:55:05 -0300

    My first code in Terraform

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.14.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "teste01"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-21651465161321621-rubens"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Manageby = "Terraform"
+  }
+}
